232c9c58f75d71399f5f5348cb020cc426bc6d55
Leonid <leonid.sobolev285@gmail.com>
2017-09-25 15:58:08 +0300

    Added login support

--- grow/forms.py
+++ grow/forms.py
@@ -1,15 +1,16 @@
+# coding=utf-8
 from django import forms
 
 
 class RegistrationForm(forms.Form):
-    username = forms.CharField(min_length=5, label='Имя пользователя (логин): ')
+    username = forms.CharField(min_length=5, label='Имя пользователя (логин)')
     email = forms.EmailField()
-    firstname = forms.CharField(label='Имя: ')
-    lastname = forms.CharField(label='Фамилия: ')
-    password = forms.CharField(min_length=8, label='Пароль: ', widget=forms.PasswordInput)
-    confirmpass = forms.CharField(min_length=8, label='Подтвердите пароль: ', widget=forms.PasswordInput)
+    firstname = forms.CharField(label='Имя')
+    lastname = forms.CharField(label='Фамилия')
+    password = forms.CharField(min_length=8, label='Пароль', widget=forms.PasswordInput)
+    confirmpass = forms.CharField(min_length=8, label='Подтвердите пароль', widget=forms.PasswordInput)
 
 
 class LoginForm(forms.Form):
-    username_or_email = forms.CharField(label='Имя пользователя (эл. почта): ')
-    password = forms.CharField(label='Пароль: ', widget=forms.PasswordInput)
+    username_or_email = forms.CharField(label='Имя пользователя (эл. почта)')
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
--- grow/views.py
+++ grow/views.py
@@ -7,6 +7,7 @@ from django.http import HttpResponseRedirect
 from django.template import RequestContext, loader
 from .models import Article
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 import forms
 
 # Create your views here..
@@ -24,50 +25,81 @@ def index(request):
 
 def register(request):
     errors = []
-    formdata = {}
     if request.method == 'POST':
         form = forms.RegistrationForm(request.POST)
         if form.is_valid():
-            username = form.username
-            email = form.email
-            firstname = form.firstname
-            lastname = form.lastname
-            password = form.password
-            confirmpass = form.confirmpass
-
-            formdata['firstname'] = firstname
-            formdata['lastname'] = lastname
-            formdata['password'] = password
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            firstname = form.cleaned_data['firstname']
+            lastname = form.cleaned_data['lastname']
+            password = form.cleaned_data['password']
+            confirmpass = form.cleaned_data['confirmpass']
 
             if password != confirmpass:
                 errors.append("Пароли не совпадают")
 
-            sameusers = []
+            data = form.data.copy()
+            same = None
             try:
-                sameusers.append(User.objects.get(username=username))
+                same = User.objects.get(username=username)
             except User.DoesNotExist:
-                formdata['username'] = username
+                pass
+            if same:
+                errors.append("Пользователь с таким именем уже существует")
+                data['username'] = ""
+
+            same = None
             try:
-                sameusers.append(User.objects.get(email=email))
+                same = User.objects.get(email=email)
             except User.DoesNotExist:
-                formdata['email'] = email
-
-            if sameusers:
-                errors.append("Пользователь с таким именем или адресом эл. почты уже существует")
-
+                pass
+            if same:
+                errors.append("Пользователь с таким адресом эл. почты уже существует")
+                data['email'] = ""
+            form.data = data
             if errors:
-                return render(request, 'register.html', {'errors': errors, 'formdata': formdata})
+                return render(request, 'register.html', {'form': form,
+                                                         'errors': errors})
 
-            User.objects.create_user(username=username, email=email, password=password)
+            User.objects.create_user(username=username, email=email, password=password, first_name=firstname, last_name=lastname)
             return HttpResponseRedirect("/login/")
 
-    return render(request, 'register.html', {'errors': [], 'formdata': formdata})
+    return render(request, 'register.html', {'form': forms.RegistrationForm(), 'errors': []})
 
 
 def login(request):
+    errors = []
+    if request.method == 'POST':
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            username_or_email = form.cleaned_data['username_or_email']
+            password = form.cleaned_data['password']
+            user = None
+            try:
+                user = User.objects.get(username=username_or_email)
+            except User.DoesNotExist:
+                try:
+                    user = User.objects.get(username=username_or_email)
+                except User.DoesNotExist:
+                    errors.append("Пользователя с таким именем или адресом эл. почты не существует")
+                    return render(request, 'login.html', {'errors': errors})
+
+            user = authenticate(user.username, password)
+            if user is not None:
+                login(user=user)
+                return HttpResponseRedirect("/success/")
+            else:
+                errors.append("Пароль неверен")
+                return render(request, 'login.html', {'errors': errors})
 
     return render(request, 'login.html')
 
+
+def success(request):
+    if request.user.is_authenticated():
+        return render(request, 'success.html', {"success": True, "username": request.user.username})
+    else:
+        return render(request, 'success.html', {"success": False})
 
 def article(request, id):
     art = Article.objects.get(id=int(id))
